fbdbb13cefc2d46ba497352207c8e9ffedb89f03
Sunny Bains <sunny-gitlab@bains.org>
2024-04-05 20:48:09 -0700

    Clean up

--- include/row0merge.h
+++ include/row0merge.h
@@ -1,4 +1,4 @@
-/**
+/****************************************************************************
 Copyright (c) 2005, 2010, Innobase Oy. All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it under
@@ -21,155 +21,188 @@ Index build routines using a merge sort
 Created 13/06/2005 Jan Lindstrom
 *******************************************************/
 
-#ifndef row0merge_h
-#define row0merge_h
+#pragma once
 
-#include "btr0types.h"
-#include "data0data.h"
-#include "dict0types.h"
 #include "innodb0types.h"
+
 #include "lock0types.h"
-#include "mtr0mtr.h"
-#include "que0types.h"
-#include "read0types.h"
-#include "rem0rec.h"
-#include "rem0types.h"
 #include "row0types.h"
 #include "srv0srv.h"
-#include "trx0types.h"
+
+struct trx_t;
+struct dict_table_t;
 
 /** Index field definition */
-struct merge_index_field_struct {
-  ulint prefix_len;       /*!< column prefix length, or 0
-                          if indexing the whole column */
-  const char *field_name; /*!< field name */
-};
+struct merge_index_field_t {
+  /** Column prefix length, or 0 if indexing the whole column */
+  ulint prefix_len;
 
-/** Index field definition */
-typedef struct merge_index_field_struct merge_index_field_t;
-
-/** Definition of an index being created */
-struct merge_index_def_struct {
-  const char *name;            /*!< index name */
-  ulint ind_type;              /*!< 0, DICT_UNIQUE,
-                               or DICT_CLUSTERED */
-  ulint n_fields;              /*!< number of fields
-                               in index */
-  merge_index_field_t *fields; /*!< field definitions */
+  /** Field name */
+  const char *field_name;
 };
 
 /** Definition of an index being created */
-typedef struct merge_index_def_struct merge_index_def_t;
+struct merge_index_def_t {
+  /** Index name */
+  const char *name;
 
-/** Sets an exclusive lock on a table, for the duration of creating indexes.
-@return	error code or DB_SUCCESS */
+  /** *  0, DICT_UNIQUE, or DICT_CLUSTERED */
+  ulint ind_type;
 
-db_err row_merge_lock_table(
-  trx_t *trx,          /*!< in/out: transaction */
-  dict_table_t *table, /*!< in: table to lock */
-  enum Lock_mode mode
-); /*!< in: LOCK_X or LOCK_S */
-/** Drop an index from the InnoDB system tables.  The data dictionary must
-have been locked exclusively by the caller, because the transaction
-will not be committed. */
+  /** Number of fields in index */
+  ulint n_fields;
 
-void row_merge_drop_index(
-  dict_index_t *index, /*!< in: index to be removed */
-  dict_table_t *table, /*!< in: table */
-  trx_t *trx
-); /*!< in: transaction handle */
-/** Drop those indexes which were created before an error occurred when
-building an index.  The data dictionary must have been locked
-exclusively by the caller, because the transaction will not be
-committed. */
+  /** field definitions */
+  merge_index_field_t *fields;
+};
 
-void row_merge_drop_indexes(
-  trx_t *trx,           /*!< in: transaction */
-  dict_table_t *table,  /*!< in: table containing the indexes */
-  dict_index_t **index, /*!< in: indexes to drop */
-  ulint num_created
-); /*!< in: number of elements in index[] */
-/** Drop all partially created indexes during crash recovery. */
+/**
+ * Sets an exclusive lock on a table, for the duration of creating indexes.
+ *
+ * @param trx   in/out: transaction
+ * @param table in: table to lock
+ * @param mode  in: LOCK_X or LOCK_S
+ *
+ * @return error code or DB_SUCCESS
+ */
+db_err row_merge_lock_table(trx_t *trx, dict_table_t *table, Lock_mode mode);
 
-void row_merge_drop_temp_indexes(ib_recovery_t recovery); /*!< in: recovery level setting */
-/** Rename the tables in the data dictionary.  The data dictionary must
-have been locked exclusively by the caller, because the transaction
-will not be committed.
-@return	error code or DB_SUCCESS */
+/**
+ * Drop an index from the InnoDB system tables. The data dictionary must
+ * have been locked exclusively by the caller, because the transaction
+ * will not be committed.
+ *
+ * @param index in: index to be removed
+ * @param table in: table
+ * @param trx   in: transaction handle
+ */
+void row_merge_drop_index(dict_index_t *index, dict_table_t *table, trx_t *trx);
 
+/**
+ * Drop those indexes which were created before an error occurred when
+ * building an index. The data dictionary must have been locked
+ * exclusively by the caller, because the transaction will not be
+ * committed.
+ *
+ * @param trx         in: transaction
+ * @param table       in: table containing the indexes
+ * @param index       in: indexes to drop
+ * @param num_created in: number of elements in index[]
+ */
+void row_merge_drop_indexes(trx_t *trx, dict_table_t *table, dict_index_t **index, ulint num_created);
+
+/**
+ * Drop all partially created indexes during crash recovery.
+ *
+ * @param recovery in: recovery level setting
+ */
+void row_merge_drop_temp_indexes(ib_recovery_t recovery);
+
+/**
+ * Rename the tables in the data dictionary. The data dictionary must
+ * have been locked exclusively by the caller, because the transaction
+ * will not be committed.
+ *
+ * @param old_table in/out: old table, renamed to tmp_name
+ * @param new_table in/out: new table, renamed to old_table->name
+ * @param tmp_name  in: new name for old_table
+ * @param trx       in: transaction handle
+ *
+ * @return error code or DB_SUCCESS
+ */
 db_err row_merge_rename_tables(
-  dict_table_t *old_table, /*!< in/out: old table, renamed
-                                                 to tmp_name */
-  dict_table_t *new_table, /*!< in/out: new table, renamed
-                                                 to old_table->name */
-  const char *tmp_name,    /*!< in: new name for old_table */
+  dict_table_t *old_table,
+  dict_table_t *new_table,
+  const char *tmp_name,
   trx_t *trx
-); /*!< in: transaction handle */
+);
 
-/** Create a temporary table for creating a primary key, using the definition
-of an existing table.
-@return	table, or nullptr on error */
-
+/**
+ * Create a temporary table for creating a primary key, using the definition
+ * of an existing table.
+ *
+ * @param table_name in: new table name
+ * @param index_def  in: the index definition of the primary key
+ * @param table      in: old table definition
+ * @param trx        in/out: transaction (sets error_state)
+ *
+ * @return table, or nullptr on error
+ */
 dict_table_t *row_merge_create_temporary_table(
-  const char *table_name,             /*!< in: new table name */
-  const merge_index_def_t *index_def, /*!< in: the index definition
-                                        of the primary key */
-  const dict_table_t *table,          /*!< in: old table definition */
+  const char *table_name,
+  const merge_index_def_t *index_def,
+  const dict_table_t *table,
   trx_t *trx
-); /*!< in/out: transaction
-                                        (sets error_state) */
+);
 
-/** Rename the temporary indexes in the dictionary to permanent ones.  The
-data dictionary must have been locked exclusively by the caller,
-because the transaction will not be committed.
-@return	DB_SUCCESS if all OK */
+/**
+ * Rename the temporary indexes in the dictionary to permanent ones. The
+ * data dictionary must have been locked exclusively by the caller,
+ * because the transaction will not be committed.
+ *
+ * @param trx   in/out: transaction
+ * @param table in/out: table with new indexes
+ *
+ * @return DB_SUCCESS if all OK
+ */
+db_err row_merge_rename_indexes( trx_t *trx, dict_table_t *table);
 
-db_err row_merge_rename_indexes(
-  trx_t *trx, /*!< in/out: transaction */
-  dict_table_t *table
-); /*!< in/out: table with new indexes */
-
-/** Create the index and load in to the dictionary.
-@return	index, or nullptr on error */
-
+/**
+ * Create the index and load in to the dictionary.
+ *
+ * @param trx       in/out: trx (sets error_state)
+ * @param table     in: the index is on this table
+ * @param index_def in: the index definition
+ *
+ * @return index, or nullptr on error
+ */
 dict_index_t *row_merge_create_index(
-  trx_t *trx,          /*!< in/out: trx (sets error_state) */
-  dict_table_t *table, /*!< in: the index is on this table */
+  trx_t *trx,
+  dict_table_t *table,
   const merge_index_def_t *index_def
 );
-/*!< in: the index definition */
-/** Check if a transaction can use an index.
-@return	true if index can be used by the transaction else false */
 
-bool row_merge_is_index_usable(
-  const trx_t *trx, /*!< in: transaction */
-  const dict_index_t *index
-); /*!< in: index to check */
-/** If there are views that refer to the old table name then we "attach" to
-the new instance of the table else we drop it immediately.
-@return	DB_SUCCESS or error code */
+/**
+ * Check if a transaction can use an index.
+ *
+ * @param trx   in: transaction
+ * @param index in: index to check
+ *
+ * @return true if index can be used by the transaction else false
+ */
+bool row_merge_is_index_usable(const trx_t *trx, const dict_index_t *index);
 
-db_err row_merge_drop_table(
-  trx_t *trx, /*!< in: transaction */
-  dict_table_t *table
-); /*!< in: table instance to drop */
+/**
+ * If there are views that refer to the old table name then we "attach" to
+ * the new instance of the table else we drop it immediately.
+ *
+ * @param trx   in: transaction
+ * @param table in: table instance to drop
+ *
+ * @return DB_SUCCESS or error code
+ */
+db_err row_merge_drop_table(trx_t *trx, dict_table_t *table);
 
-/** Build indexes on a table by reading a clustered index,
-creating a temporary file containing index entries, merge sorting
-these index entries and inserting sorted index entries to indexes.
-@return	DB_SUCCESS or error code */
-
+/**
+ * Build indexes on a table by reading a clustered index,
+ * creating a temporary file containing index entries, merge sorting
+ * these index entries and inserting sorted index entries to indexes.
+ *
+ * @param trx        in: transaction
+ * @param old_table  in: table where rows are read from
+ * @param new_table  in: table where indexes are created; identical to old_table
+ *                      unless creating a PRIMARY KEY
+ * @param indexes    in: indexes to be created
+ * @param n_indexes  in: size of indexes[]
+ * @param table      in/out: table, for reporting erroneous key value if applicable
+ *
+ * @return DB_SUCCESS or error code
+ */
 db_err row_merge_build_indexes(
-  trx_t *trx,              /*!< in: transaction */
-  dict_table_t *old_table, /*!< in: table where rows are
-                             read from */
-  dict_table_t *new_table, /*!< in: table where indexes are
-                             created; identical to old_table
-                             unless creating a PRIMARY KEY */
-  dict_index_t **indexes,  /*!< in: indexes to be created */
-  ulint n_indexes,         /*!< in: size of indexes[] */
+  trx_t *trx,
+  dict_table_t *old_table,
+  dict_table_t *new_table,
+  dict_index_t **indexes,
+  ulint n_indexes,
   table_handle_t table
-);     /*!< in/out: table, for
-                             reporting erroneous key value
-                             if applicable */
-#endif /* row0merge.h */
+);
